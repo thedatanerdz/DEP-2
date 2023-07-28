5734adb241b45d176cf2ebd0523fb1c0bf4135c7
Soleil Botha <122761529+thedatanerdz@users.noreply.github.com>
2023-07-28 04:56:29 +0200

    Add files via upload

---
+++ q7.sql
@@ -0,0 +1,6 @@
+select f.tile, sum(p.amount) as revenue from payment as p
+JOIN rental r ON p.rental_id = r.rental_id
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN film f ON i.film_id = f.film_id
+GROUP BY f.tile
+ORDER BY sum(p.amount) DESC
